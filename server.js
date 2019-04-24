a95b1f8234e39d7fb361682aeb8ac525438397a6
dli24 <dli24@buffalo.edu>
2019-04-24 14:08:27 -0700

    Initial commits, adds server boilder plate

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// ============================Middle Ware=========================//
+//Parse URL ENCODED
+app.use(bodyParser.urlencoded({extened: true}));
+// Parse JSON DATA
+app.use(bodyParser.json());
+
+//Serve Public Directory
+app.use(express.static(__dirname + '/public'));
+//=============================ROUTE==============================//
+//GET ROOT ROUTE
+app.get('/', (req,res)=>{
+    res.send('<h1>Auth APP</h1>');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//======================Start Server==============================//
+app.listen(PORT, ()=> console.log(`Server Started on PORT ${PORT}`))
